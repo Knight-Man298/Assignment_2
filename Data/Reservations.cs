2db64e92200ce098d4d1fba2d09a52948928604a
samkebert@gmail.com <samkebert@gmail.com>
2023-03-13 15:47:25 -0600

    c# changes

--- Data/Reservations.cs
+++ Data/Reservations.cs
@@ -8,24 +8,31 @@ namespace Assignment_2.Data
 {
     public class Reservations 
     {
-        private string reservationcode;
+        private string reservationCode;
+        private string flightCode;
+        private string airlineName;
+        private int cost;
         private string name;
         private string citizenship;
-        private bool status; 
+        private bool status;
 
-
-        public string Reservationcode { get => reservationcode; set => reservationcode = value; }
-        public string Name { get => name; set => name = value; }
-        public string Citizenship { get => citizenship; set => citizenship = value; }
-        public bool Status { get => status; set => status = value; }
-
-        public Reservations(string reservationcode, string name, string citizenship, bool status)
+        public Reservations(string reservationCode, string flightCode, string airlineName, int cost, string name, string citizenship, bool status)
         {
-            this.reservationcode = reservationcode;
+            this.reservationCode = reservationCode;
+            this.flightCode = flightCode;
+            this.airlineName = airlineName;
+            this.cost = cost;
             this.name = name;
             this.citizenship = citizenship;
             this.status = status;
-
         }
+
+        public string ReservationCode { get => reservationCode; set => reservationCode = value; }
+        public string FlightCode { get => flightCode; set => flightCode = value; }
+        public string AirlineName { get => airlineName; set => airlineName = value; }
+        public int Cost { get => cost; set => cost = value; }
+        public string Name { get => name; set => name = value; }
+        public string Citizenship { get => citizenship; set => citizenship = value; }
+        public bool Status { get => status; set => status = value; }
     }
 }
