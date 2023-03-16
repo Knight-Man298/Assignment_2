2340e838695a67db151025404516a3283cd1115f
samkebert@gmail.com <samkebert@gmail.com>
2023-03-16 10:23:48 -0600

    variable changes

--- Data/Flights.cs
+++ Data/Flights.cs
@@ -4,33 +4,36 @@ public class Flights
 {
     public string flightCode;
 	public string airlineName;
-	public string startingPoint;
+	public string originAirport;
     public string destination;
     public string day;
 	public string time;
 	public int availableSeats;
-	public int price;
+    public int totalSeats; 
+	public double price;
 
-    public Flights(string flightcode, string airlinename, string startingpoint, string destination, string day, string time, int seats, int price)
+    public Flights(string flightcode, string airlinename, string originairport, string destination, string day, string time, int seats,int totalseats ,double price)
     {
         this.flightCode = flightcode;
         this.airlineName = airlinename;
-        this.startingPoint = startingpoint;
+        this.originAirport = originairport;
         this.destination = destination;
         this.day = day;
         this.time = time;
         this.availableSeats = seats;
+        this.totalSeats = totalseats;
         this.price = price;
     }
 
     public string Flightcode { get => flightCode; set => flightCode = value; }
     public string AirLineName { get=> airlineName; set=> airlineName = value; }
-    public string StartingPoint { get => startingPoint; set => startingPoint = value; }
+    public string OriginAirport{ get => originAirport; set => originAirport = value; }
     public string Destination { get => destination; set => destination = value; }
     public string Day { get => day; set => day = value; }
     public string Time { get => time; set => time = value; }
     public int AvailableSeats { get => availableSeats; set => availableSeats = value; }
-    public int Price { get => price; set => price = value; }
+    public int TotalSeats { get => totalSeats; set => totalSeats = value; }
+    public double Price { get => price; set => price = value; }
 
     public void FindFlight()
     {
