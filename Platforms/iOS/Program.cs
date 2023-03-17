e110a0bac5a0932b97bd4cf2933246211e408d39
samkebert@gmail.com <samkebert@gmail.com>
2023-03-16 18:26:50 -0600

    bruh

--- Platforms/iOS/Program.cs
+++ Platforms/iOS/Program.cs
@@ -3,13 +3,13 @@ using UIKit;
 
 namespace Assignment_2;
 
-//public class Program
-//{
+public class Program
+{
 	// This is the main entry point of the application.
-	//static void Main(string[] args)
-	//{
+	static void Main(string[] args)
+	{
 		// if you want to use a different Application Delegate class from "AppDelegate"
 		// you can specify it here.
-		//UIApplication.Main(args, null, typeof(AppDelegate));
-	//}
-//}
+		UIApplication.Main(args, null, typeof(AppDelegate));
+	}
+}
